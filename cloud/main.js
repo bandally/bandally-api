ba76f7b407c183a22103447c220b85e879c8e270
yosket <yosket87@gmail.com>
2016-05-12 14:28:44 +0900

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,5 @@
 Parse.Cloud.define('getBase64StringFromUrl', function (req, res) {
+  Parse.Cloud.useMasterKey();
   console.log(req.params);
   Parse.Cloud.httpRequest({
     url: req.params.imageUrl
