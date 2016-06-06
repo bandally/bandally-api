1ad398b7002992aecd35cb79a35379fb65aa3c12
Yosuke Tanaka <yosket87@gmail.com>
2016-06-06 13:02:43 +0900

    ログを消す

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ var api = new ParseServer({
   },
   filesAdapter: filesAdapter
 });
-console.log(api);
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
